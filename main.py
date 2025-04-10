ab2f2a021627873a17c46850c5fdf0a147ad8b80
Ten2005 <stuzeukni0901@icloud.com>
2025-04-10 14:25:53 +0900

    adjust port

--- main.py
+++ main.py
@@ -71,4 +71,5 @@ async def feedback(request: schemas.FeedbackRequest):
     return {"status": "success"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
